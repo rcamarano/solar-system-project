aed24516f9db533a5feca412198e2e17b8736bc5
Rogerio Camarano <rogeriocamarano@gmail.com>
2022-11-25 15:36:42 -0300

    req 1 and 2 done

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (<div data-testid="solar-system" />);
+  }
+}
+
+export default SolarSystem;
